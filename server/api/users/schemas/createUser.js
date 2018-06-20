2099659f0a2047aa5da8a28137c7647ccb1cbece
Rohit Prasad <rohitrp3@gmail.com>
2018-06-21 03:40:01 +0530

    Refactor file

--- server/api/users/schemas/createUser.js
+++ server/api/users/schemas/createUser.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-const createUserSchema = Joi.object({
+const createUserSchema = Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).max(30).required()
 })
