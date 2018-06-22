f3185cc33399a28896a3ceb6a617b2ee7a24c9fa
Rohit Prasad <rohitrp3@gmail.com>
2018-06-23 00:35:36 +0530

    Implement hero banner

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() subtitle?: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
