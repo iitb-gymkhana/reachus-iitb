60f252fbe090a5053d187ac6eec0c6701ae762bd
Rohit Prasad <rohitrp3@gmail.com>
2018-06-25 22:45:32 +0530

    Add component for show using bookings

---
+++ src/app/bookings/user-bookings/user-bookings.component.ts
@@ -0,0 +1,16 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-bookings',
+  templateUrl: './user-bookings.component.html',
+  styleUrls: ['./user-bookings.component.css']
+})
+export class UserBookingsComponent implements AfterViewInit {
+
+  constructor() { }
+
+  ngAfterViewInit() {
+
+  }
+
+}
