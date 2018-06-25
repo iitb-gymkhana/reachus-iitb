623eba98ae777537776a42afc2b57a6d9a72e66a
Rohit Prasad <rohitrp3@gmail.com>
2018-06-26 02:59:50 +0530

    Make api call to create booking

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,25 +1,38 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import bulmaCalendar from 'node_modules/bulma-calendar/dist/js/bulma-calendar.min.js';
 import * as moment from 'moment';
 import { AlertService } from '../../_services/alert.service';
+import { Room } from '../../room';
+import { RoomService } from '../../_services/room.service';
+import { BookingService } from '../../_services/booking.service';
 
 @Component({
   selector: 'app-create-booking',
   templateUrl: './create-booking.component.html',
   styleUrls: ['./create-booking.component.css']
 })
-export class CreateBookingComponent implements AfterViewInit {
+export class CreateBookingComponent implements OnInit, AfterViewInit {
+  rooms: any;
 
-  onSubmit(startDate, startTime, endDate, endTime) {
+  onSubmit(startDate, startTime, endDate, endTime, roomId) {
     if (startDate && startTime && endDate && endTime) {
       const startDateTime = moment.utc(`${startDate} ${startTime}`, 'YYYY-MM-DD HH:mm');
       const endDateTime = moment.utc(`${endDate} ${endTime}`, 'YYYY-MM-DD HH:mm');
 
-      console.log(startDateTime.format());
-      console.log(endDateTime.format());
-
       if (startDateTime.isAfter(endDateTime)) {
         this.alertService.error('The starting date/time of booking cannot be after end date/time of booking');
+      } else {
+        this.bookingService.createBooking(
+          {
+            from: startDateTime.format(),
+            to: endDateTime.format(),
+            room: roomId
+          }
+        )
+        .subscribe(
+          (res) => this.alertService.success(res.message),
+          (err) => this.alertService.error(err)
+        );
       }
 
     } else {
@@ -28,8 +41,18 @@ export class CreateBookingComponent implements AfterViewInit {
   }
 
   constructor(
-    private alertService: AlertService
+    private alertService: AlertService,
+    private roomService: RoomService,
+    private bookingService: BookingService
   ) { }
+
+  ngOnInit() {
+    this.roomService.getAllRooms()
+      .subscribe(
+        (res) => this.rooms = res,
+        (err) => this.alertService.error(err)
+      );
+  }
 
   ngAfterViewInit() {
     document.addEventListener('DOMContentLoaded', function () {
