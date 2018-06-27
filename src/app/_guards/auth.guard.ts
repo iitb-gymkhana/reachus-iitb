00d2f42ce73e4259d4952a54c0f59bc8621ff280
Rohit Prasad <rohitrp3@gmail.com>
2018-06-28 03:14:05 +0530

    Check if token is expired in auth guard

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (localStorage.getItem('user')) {
+    if (this.authService.isAuthenticated()) {
       return true;
     }
 
