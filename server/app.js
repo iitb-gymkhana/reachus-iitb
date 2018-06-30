b34cd92d6b7fa628e04088ba06089baf20a4d5bb
Rohit Prasad <rohitrp3@gmail.com>
2018-06-30 17:56:40 +0530

    Get paths for swagger documentation from env variables

--- server/app.js
+++ server/app.js
@@ -19,6 +19,9 @@ const DB_PASS = process.env.DB_PASS;
 const DB_PORT = process.env.DB_PORT;
 const SERVER_PORT = process.env.SERVER_PORT || 3000
 const SERVER_HOST = process.env.SERVER_HOST || 'localhost'
+const SWAGGER_JSON_PATH = process.env.SWAGGER_JSON_PATH || '/swagger.json'
+const SWAGGER_DOCUMENTATION_PATH = process.env.SWAGGER_DOCUMENTATION_PATH || '/documentation'
+const SWAGGER_UI_PATH = process.env.SWAGGER_UI_PATH || '/swaggerui/'
 
 const dbUrl = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/SAC`;
 
@@ -42,13 +45,14 @@ const init = async () => {
             }
         }
     });
-
     const swaggerOptions = {
         info: {
             title: 'API Documentation',
             version: Pack.version,
         },
-        documentationPath: '/sac/booking/api/documentation'
+        jsonPath: SWAGGER_JSON_PATH,
+        documentationPath: SWAGGER_DOCUMENTATION_PATH,
+        swaggerUIPath: SWAGGER_UI_PATH
     };
 
     await server.register(require('hapi-auth-jwt2'));
