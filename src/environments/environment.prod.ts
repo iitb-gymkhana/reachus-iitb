433697b5e2b6b200a59de3ce67cbed3e8602e4ab
Rohit Prasad <rohitrp3@gmail.com>
2018-09-16 01:59:25 +0530

    Fix the login url in env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://gymkhana.iitb.ac.in/portal/sac/api',
-  loginUrl: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/?client_id=1pf0juidhJgvSNzF1UG9VJ1xUA6hIplm0ByZyYws&response_type=code&scope=profile%20ldap'
+  loginUrl: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/?client_id=1pf0juidhJgvSNzF1UG9VJ1xUA6hIplm0ByZyYws&response_type=code&scope=profile%20ldap%20send_mail'
 };
