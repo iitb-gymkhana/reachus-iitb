11aca590b4421a1aebe82c892d5b8b5c0fb0a612
Rohit Prasad <rohitrp3@gmail.com>
2018-09-17 01:36:31 +0530

    Add api to get user details

---
+++ server/api/users/routes/getUserDetails.js
@@ -0,0 +1,25 @@
+const User = require('../../users/model/User')
+const Boom = require('boom')
+
+module.exports = {
+  method: 'GET',
+  path: '/api/users/{user}',
+  options: {
+    handler: async (request, h) => {
+      const user = User.findOne({ldap_username: request.params.user}).lean()
+      
+      if (!user) {
+        return Boom.notFound(`No user with username ${request.params.user} present`)
+      }
+      
+      return user
+    },
+    auth: {
+      strategy: 'jwt',
+      scope: ['superuser']
+    },
+    description: 'Gets user\'s details',
+    notes: 'Gets user\'s details',
+    tags: ['api', 'user', 'superuser']
+  }
+}
