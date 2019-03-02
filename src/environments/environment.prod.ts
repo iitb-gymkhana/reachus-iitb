bb87e9055791f6f8603a92543efb6a04bfa09b6b
Rohit Prasad <rohitrp3@gmail.com>
2019-03-02 20:51:05 +0530

    Fix base api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://gymkhana.iitb.ac.in/portal/offer/api',
+  apiBaseUrl: 'https://gymkhana.iitb.ac.in/portal/reachus/api',
   loginUrl: 'https://gymkhana.iitb.ac.in/sso/oauth/authorize/?client_id=SJJgdqKpK7hisYfyFtVcfpAOqUQWjE5lU9sXkwXq&response_type=code&scope=profile%20ldap%20send_mail'
 };
